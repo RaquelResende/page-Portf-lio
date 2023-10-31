452aef7fccb7eb15541600120414219ff4feff22
unknown <raquelresende_@outlook.com>
2023-10-31 19:34:00 -0300

    page third-commit

---
+++ index.js
@@ -0,0 +1,22 @@
+(()=> {
+  
+  const abas = document.querySelectorAll("[data-aba]")
+
+  esconderConteudo =()=>{
+  const conteudo = document.querySelectorAll("[data-conteudo]")
+
+  conteudo.forEach((conteudo)=> conteudo.classList.add("hide"))
+  }
+
+  abas.forEach((aba) =>
+  aba.addEventListener("click", ()=>{
+
+
+  }
+  
+  )
+  )
+
+  
+  
+})();
